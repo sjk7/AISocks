fe83ea084c1f53640db730021b5c0e4f4491f478
Cool-Coolie <radiowebmasters@gmail.com>
2026-02-21 11:46:23 +0000

    perf: Add precompiled headers and enable maximum parallelization
    Created PCH with expensive headers for 19% faster builds (13.1s to 10.6s)

---
+++ lib/src/pch.cpp
@@ -0,0 +1,6 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java:
+// https://pvs-studio.com
+// Precompiled header implementation file
+
+#include "pch.h"
